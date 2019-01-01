347812226bb0a8aef272345c4d140734f1695fc7
Bradley Boggs <bradleywboggs@gmail.com>
2019-01-01 16:48:21 -0600

    filled in initial view for user/add

--- src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
@@ -1,10 +1,19 @@
 package org.launchcode.cheesemvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+    @RequestMapping(value = "add", method  = RequestMethod.GET )
+    public String add(Model model) {
+        model.addAttribute("title", "Sign in");
+        return "user/add";
+    }
+
 
 }
