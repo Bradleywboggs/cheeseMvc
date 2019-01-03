d60d412ddd5acb1a88ca43e3b8d8ff91cfcf040f
Bradley Boggs <bradleywboggs@gmail.com>
2019-01-02 20:16:29 -0600

    validated user sign up form. Added welcome page and sign up link

--- src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
@@ -1,19 +1,34 @@
 package org.launchcode.cheesemvc.controllers;
 
+import org.launchcode.cheesemvc.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-    @RequestMapping(value = "add", method  = RequestMethod.GET )
-    public String add(Model model) {
-        model.addAttribute("title", "Sign in");
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddForm(Model model) {
+        model.addAttribute("title", "Sign Up");
         return "user/add";
     }
 
-
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddForm(Model model, @ModelAttribute User user, @RequestParam String verify) {
+        if (verify.equals(user.getPassword())) {
+            model.addAttribute("user", user);
+            model.addAttribute("title", "Welcome");
+            return "user/index";
+        } else {
+            model.addAttribute("pwErrorMessage", "Passwords did not match.");
+            model.addAttribute("userEmail", user.getEmail());
+            model.addAttribute("username", user.getUsername());
+            return "user/add";
+        }
+    }
 }
